4f1e8a4bf9a029fa8a94077c93c88b93c587ab7a
Chanaka Sanjeewa <Chanakasa@hbs.com>
2021-06-02 17:06:39 +0530

    first commit

---
+++ precache-manifest.b10f98831477f9ca5dc4a5906eb0c201.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98b7e5e79e7bffd458d892ed4cc810e",
+    "url": "/React-h-biz/index.html"
+  },
+  {
+    "revision": "a4de01b41687954d6b94",
+    "url": "/React-h-biz/static/css/main.afea90cd.chunk.css"
+  },
+  {
+    "revision": "a5bd4f58a8d0e5c24ac7",
+    "url": "/React-h-biz/static/js/2.89634bec.chunk.js"
+  },
+  {
+    "revision": "a4de01b41687954d6b94",
+    "url": "/React-h-biz/static/js/main.16468f58.chunk.js"
+  },
+  {
+    "revision": "44b2146496d80bfe947f",
+    "url": "/React-h-biz/static/js/runtime-main.ad403ee1.js"
+  }
+]);
